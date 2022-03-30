79b4db94d2bb5e0be898414d80047c7503db7b70
Daniel Saiz <danisaiz@ucm.es>
2022-03-30 12:24:27 +0200

    multiplayer game

--- multiplayer.py
+++ multiplayer.py
@@ -48,11 +48,18 @@ def draw_lives(surf, x, y, lives, img):
         img_rect.y = y
         surf.blit(img, img_rect)
 
-def show_go_screen():
+def show_go_screen(winner):
     screen.blit(background, background_rect)
-    draw_text(screen, "SHMUP!", 64, WIDTH / 2, HEIGHT / 4)
-    draw_text(screen, "Arrow keys move, Space to fire", 22,
-              WIDTH / 2, HEIGHT / 2)
+    if winner == 0:
+        draw_text(screen, "MORTAL SPACE KOMBAT!", 64, WIDTH / 2, HEIGHT / 4)
+    if winner == 1:
+        draw_text(screen, "PLAYER 1 WINS!", 64, WIDTH / 2, HEIGHT / 4)
+        draw_text(screen, "Play again?", 18, WIDTH / 2, HEIGHT * 3 / 4 -20)
+    if winner == 2:
+        draw_text(screen, "PLAYER 2 WINS!", 64, WIDTH / 2, HEIGHT / 4)
+        draw_text(screen, "Play again?", 18, WIDTH / 2, HEIGHT * 3 / 4 -20)
+    draw_text(screen, "P1: up, down arrow keys to move, space to fire", 22, WIDTH / 2, HEIGHT / 2-15)
+    draw_text(screen, "P2: w, s keys to move, m to fire", 22, WIDTH / 2, HEIGHT / 2+15)
     draw_text(screen, "Press a key to begin", 18, WIDTH / 2, HEIGHT * 3 / 4)
     pg.display.flip()
     waiting = True
@@ -72,7 +79,7 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.radius = 21
         #pg.draw.circle(self.image, RED, self.rect.center, self.radius)
-        self.rect.centerx = 10
+        self.rect.centerx = 20
         self.rect.bottom = HEIGHT/2
         self.speedy = 0
         self.live = 3
@@ -80,9 +87,9 @@ class Player(pg.sprite.Sprite):
     def update(self):
         self.speedy = 0
         keystate = pg.key.get_pressed()
-        if keystate[pg.K_LEFT]:
+        if keystate[pg.K_UP]:
             self.speedy = -8
-        if keystate[pg.K_RIGHT]:
+        if keystate[pg.K_DOWN]:
             self.speedy = 8
         self.rect.y += self.speedy
         if self.rect.bottom >= HEIGHT:
@@ -91,11 +98,40 @@ class Player(pg.sprite.Sprite):
             self.rect.top = 0
         
     def shoot(self):
-        bullet = Bullet(self.rect.centerx, self.rect.top)
+        bullet = Bullet(self.rect.centerx, self.rect.top, 1)
         all_sprites.add(bullet)
         bullets.add(bullet)
 
+class Player2(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.transform.scale(player2_img, (50, 38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 21
+        #pg.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.centerx = WIDTH-20
+        self.rect.bottom = HEIGHT/2
+        self.speedy = 0
+        self.live = 3
 
+    def update(self):
+        self.speedy = 0
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_w]:
+            self.speedy = -8
+        if keystate[pg.K_s]:
+            self.speedy = 8
+        self.rect.y += self.speedy
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+        
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top, 2)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
     
 class Mob(pg.sprite.Sprite):
     def __init__(self):
@@ -118,50 +154,61 @@ class Mob(pg.sprite.Sprite):
             self.speedy = random.randrange(1, 8)
 
 class Bullet(pg.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, p):
         pg.sprite.Sprite.__init__(self)
-        self.image = bullet_img
+        if p == 1:
+            self.image = bullet_img
+        if p == 2:
+            self.image = bullet2_img
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.bottom = y+25
-        self.rect.centerx = x+25
+        self.rect.bottom = y 
+        self.rect.centerx = x
         self.speedx = 15
-
+        self.player = p
+        
     def update(self):
-        self.rect.x += self.speedx
+        if self.player == 1:
+            self.rect.x += self.speedx
+        else:
+            self.rect.x -= self.speedx
         # kill if it moves off the top of the screen
-        if (self.rect.right > WIDTH) or (self.rect.left < -25):
+        if (self.rect.right > WIDTH) or (self.rect.left < 0):
             self.kill()
 #Load graphics
 background = pg.image.load(path.join(img_dir, "b.png")).convert()
 background_rect = background.get_rect() 
 player_img = pg.image.load(path.join(img_dir, "playerShip3_orange.png")).convert()
+player2_img = pg.image.load(path.join(img_dir, "playerShip3_blue.png")).convert()
 meteor_img = pg.image.load(path.join(img_dir, "meteorBrown_big3.png")).convert()
 bullet_img = pg.image.load(path.join(img_dir, "laserRed16.png")).convert()
-#player_img = pg.image.load(path.join(img_dir, "playerShip3_orange.png")).convert()
+bullet2_img = pg.image.load(path.join(img_dir, "laserRed17.png")).convert()
 player_mini_img = pg.transform.scale(player_img, (25, 19))
 player_mini_img.set_colorkey(BLACK)
-         
+player2_mini_img = pg.transform.scale(player2_img, (25, 19))
+player2_mini_img.set_colorkey(BLACK)        
 
 # Game loop
+winner = 0
 game_over = True
 running = True
 while running:
     if game_over:
-        show_go_screen()
+        show_go_screen(winner)
         game_over = False
         all_sprites = pg.sprite.Group()
         mobs = pg.sprite.Group()
         bullets = pg.sprite.Group()
         player = Player()
+        player2 = Player2()
         all_sprites.add(player)
+        all_sprites.add(player2)
 
 # =============================================================================
 #         for i in range(8):
 #            new_mob()
 # =============================================================================
-
-        score = 0
+        
     # keep loop running at the right speed
     clock.tick(FPS)
     # Process input (events)
@@ -172,14 +219,17 @@ while running:
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
                 player.shoot()
-
+            if event.key == pg.K_m:
+                player2.shoot()
     # Update
     all_sprites.update()
-    #check if any bullet hit any mob
-    hits = pg.sprite.groupcollide(mobs, bullets, True, True)
-    for hit in hits:
-        score += 1
-        new_mob()
+# =============================================================================
+#     #check if any bullet hit any mob
+#     hits = pg.sprite.groupcollide(mobs, bullets, True, True)
+#     for hit in hits:
+#         score += 1
+#         new_mob()
+# =============================================================================
     #check to see if a mob hit the player
     hits = pg.sprite.spritecollide(player, mobs, True, pg.sprite.collide_circle)
     for hit in hits:
@@ -187,12 +237,24 @@ while running:
         new_mob()
         if player.live <= 0:
             game_over = True	
+    hits = pg.sprite.spritecollide(player, bullets, True)
+    for hit in hits:
+        player.live -= 1
+        if player.live <= 0:
+            winner = 2
+            game_over = True
+    hits = pg.sprite.spritecollide(player2, bullets, True)
+    for hit in hits:
+        player2.live -= 1
+        if player2.live <= 0:
+            winner = 1
+            game_over = True
     # Draw / render
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
-    draw_text(screen, str(score), 18, WIDTH / 2, 10)
-    draw_lives(screen, WIDTH - 100, 5, player.live, player_mini_img)
+    draw_lives(screen, 10, 5, player.live, player_mini_img)
+    draw_lives(screen, WIDTH - 100, 5, player2.live, player2_mini_img)
     # *after* drawing everything, flip the display
     pg.display.flip()
     
